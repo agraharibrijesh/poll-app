15fc451f019f7c915ac7840bcb9ec0fc4f31aa30
Agrahari <12600@intelizign.com>
2023-07-15 20:48:48 +0530

    1stcommit

---
+++ quiz-application/Quiz/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import redirect,render
+from django.contrib.auth import login,logout,authenticate
+from .forms import *
+from .models import *
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        print(request.POST)
+        questions=QuesModel.objects.all()
+        score=0
+        wrong=0
+        correct=0
+        total=0
+        for q in questions:
+            total+=1
+            print(request.POST.get(q.question))
+            print(q.ans)
+            print()
+            if q.ans ==  request.POST.get(q.question):
+                score+=10
+                correct+=1
+            else:
+                wrong+=1
+        percent = score/(total*10) *100
+        context = {
+            'score':score,
+            'time': request.POST.get('timer'),
+            'correct':correct,
+            'wrong':wrong,
+            'percent':percent,
+            'total':total
+        }
+        return render(request,'Quiz/result.html',context)
+    else:
+        questions=QuesModel.objects.all()
+        context = {
+            'questions':questions
+        }
+        return render(request,'Quiz/home.html',context)
+
+def addQuestion(request):    
+    if request.user.is_staff:
+        form=addQuestionform()
+        if(request.method=='POST'):
+            form=addQuestionform(request.POST)
+            if(form.is_valid()):
+                form.save()
+                return redirect('/')
+        context={'form':form}
+        return render(request,'Quiz/addQuestion.html',context)
+    else: 
+        return redirect('home')
+
+def registerPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            messages.error(request, 'Passwords do not match.')
+            return redirect('register')
+
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('login')
+        except:
+            messages.error(request, 'An error occurred during registration.')
+            return redirect('register')
+
+    return render(request, 'Quiz/register.html')
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+       if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+       context={}
+       return render(request,'Quiz/login.html',context)
+
+def logoutPage(request):
+    logout(request)
+    return redirect('/')
+
